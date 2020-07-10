50ec588bc0a8bc0b490da500eb1f06c32b17b7fe
Dustan.Mbaga <Dustan.Mbaga@nmbbank.co.tz>
2020-07-10 06:48:27 +0300

    Reforing code, removing unused codes and adding log

--- src/main/java/maven/dustan/io/java_read_write_excel/App.java
+++ src/main/java/maven/dustan/io/java_read_write_excel/App.java
@@ -3,6 +3,8 @@ package maven.dustan.io.java_read_write_excel;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -15,71 +17,58 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
- * Hello world!
- *
+ *  A class to read data from an excel file using Apache POI
  */
 public class App 
 {
+
 	public static final String EXCEL_FILE_PATH = "./readable_writable_excel.xlsx";
 	
-    public static void main( String[] args ) throws EncryptedDocumentException, InvalidFormatException, IOException
-    {
-        // System.out.println( "Hello World!" );
-    	// Creating a Workbook from an Excel file (.xls or .xlsx)
+    public static void main( String[] args ) throws EncryptedDocumentException, InvalidFormatException, IOException {
+        // Creating a Workbook from an Excel file (.xls or .xlsx)
         Workbook workbook = WorkbookFactory.create(new File(EXCEL_FILE_PATH));
 
-        // Retrieving the number of sheets in the Workbook
-        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
 
+        Logger logger = Logger.getLogger(App.class.getName());
+
+        // Retrieving the number of sheets in the Workbook
+        //System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        logger.info("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
         /*
            =============================================================
-           Iterating over all the sheets in the workbook (Multiple ways)
+           Iterating over all the sheets in the workbook
            =============================================================
         */
 
         // 1. You can obtain a sheetIterator and iterate over it
         Iterator<Sheet> sheetIterator = workbook.sheetIterator();
-        System.out.println("Retrieving Sheets using Iterator");
+
+        logger.info("Retrieving Sheets using Iterator:");
         while (sheetIterator.hasNext()) {
             Sheet sheet = sheetIterator.next();
             System.out.println("=> " + sheet.getSheetName());
         }
-        
-        /*
-        // 2. Or you can use a for-each loop
-        System.out.println("Retrieving Sheets using for-each loop");
-        for(Sheet sheet: workbook) {
-            System.out.println("=> " + sheet.getSheetName());
-        }
 
-        // 3. Or you can use a Java 8 forEach with lambda
-        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
-        workbook.forEach(sheet -> {
-            System.out.println("=> " + sheet.getSheetName());
-        });
-        
-        */
-
-        
         /*
            ==================================================================
-           Iterating over all the rows and columns in a Sheet (Multiple ways)
+           Iterating over all the rows and columns in a Sheet
            ==================================================================
         */
 
         // Getting the Sheet at index zero
         Sheet sheet = workbook.getSheetAt(0);
 
-        // Create a DataFormatter to format and get each cell's value as String
+        // Creating a DataFormatter to format and get each cell's value as String
         DataFormatter dataFormatter = new DataFormatter();
 
-        // 1. You can obtain a rowIterator and columnIterator and iterate over them
-        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+        // 1. Obtain a rowIterator and columnIterator and iterate over them
+        logger.info("\n\nIterating over Rows and Columns using Iterator\n");
         Iterator<Row> rowIterator = sheet.rowIterator();
         while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
 
-            // Now let's iterate over the columns of the current row
+            // iterating over the columns of the current row
             Iterator<Cell> cellIterator = row.cellIterator();
 
             while (cellIterator.hasNext()) {
@@ -89,38 +78,7 @@ public class App
             }
             System.out.println();
         }
-        
-        /*
-        // 2. Or you can use a for-each loop to iterate over the rows and columns
-        System.out.println("\n\nIterating over Rows and Columns using for-each loop\n");
-        for (Row row: sheet) {
-            for(Cell cell: row) {
-                String cellValue = dataFormatter.formatCellValue(cell);
-                System.out.print(cellValue + "\t");
-            }
-            System.out.println();
-        }
 
-        // 3. Or you can use Java 8 forEach loop with lambda
-        System.out.println("\n\nIterating over Rows and Columns using Java 8 forEach with lambda\n");
-        sheet.forEach(row -> {
-            row.forEach(cell -> {
-                String cellValue = dataFormatter.formatCellValue(cell);
-                System.out.print(cellValue + "\t");
-            });
-            System.out.println();
-        });
-        */
-        
-        /*
-        sheet.forEach(row -> {
-            row.forEach(cell -> {
-                printCellValue(cell);
-            });
-            System.out.println();
-        });
-        */
-        
         // Closing the workbook
         workbook.close();
     }
@@ -144,10 +102,9 @@ public class App
                 System.out.print(cell.getCellFormula());
                 break;
             case BLANK:
-                System.out.print("");
-                break;
             default:
                 System.out.print("");
+                break;
         }
 
         System.out.print("\t");
